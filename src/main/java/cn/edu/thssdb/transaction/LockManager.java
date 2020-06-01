ecdd2a357d2daed10a93aa57cb56b7c9cbda058a
Chinwer <qiuyh0924@163.com>
2020-06-01 16:17:14 +0800

    Lock Manager

---
+++ src/main/java/cn/edu/thssdb/transaction/LockManager.java
@@ -0,0 +1,34 @@
+package cn.edu.thssdb.transaction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LockManager {
+    private ArrayList<String> allLocks;
+    // key: session(transaction) id
+    // value: locks' name
+    private HashMap<String, ArrayList<String>> lockTable;
+
+    public boolean addLock(String sessionId, String lockName) {
+        if (allLocks.contains(lockName)) {
+            return false;
+        }
+
+        allLocks.add(lockName);
+        if (lockTable.containsKey(sessionId)) {
+            lockTable.get(sessionId).add(lockName);
+        } else {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(lockName);
+            lockTable.put(sessionId, list);
+        }
+        return true;
+    }
+
+    public void releaseLocks(String sessionId) {
+        if (lockTable.containsKey(sessionId)) {
+            lockTable.remove(sessionId);
+        }
+    }
+
+}
