8fa39b3210c37d631aa97c8e990da317d71742be
Sen Na <i@nasen.xyz>
2020-06-01 22:06:29 +0800

    Use multi-threaded server

--- src/main/java/cn/edu/thssdb/server/ThssDB.java
+++ src/main/java/cn/edu/thssdb/server/ThssDB.java
@@ -7,6 +7,7 @@ import cn.edu.thssdb.service.IServiceHandler;
 import cn.edu.thssdb.utils.Context;
 import cn.edu.thssdb.utils.Global;
 import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.server.TSimpleServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
@@ -51,7 +52,7 @@ public class ThssDB {
     private static void setUp(IService.Processor processor) {
         try {
             transport = new TServerSocket(Global.DEFAULT_SERVER_PORT);
-            server = new TSimpleServer(new TServer.Args(transport).processor(processor));
+            server = new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor));
             logger.info("Starting ThssDB ...");
             server.serve();
         } catch (TTransportException e) {
