d0acb47413d29b06829c6508fd57580cfa12f925
Sen Na <i@nasen.xyz>
2020-06-11 17:13:41 +0800

    Rename rpc api correctly (fix typos)

--- src/main/java/cn/edu/thssdb/service/IServiceHandler.java
+++ src/main/java/cn/edu/thssdb/service/IServiceHandler.java
@@ -2,15 +2,7 @@ package cn.edu.thssdb.service;
 
 import cn.edu.thssdb.parser.SQLEvalResult;
 import cn.edu.thssdb.parser.SQLEvaluator;
-import cn.edu.thssdb.rpc.thrift.ConnectReq;
-import cn.edu.thssdb.rpc.thrift.ConnectResp;
-import cn.edu.thssdb.rpc.thrift.DisconnetResp;
-import cn.edu.thssdb.rpc.thrift.ExecuteStatementReq;
-import cn.edu.thssdb.rpc.thrift.ExecuteStatementResp;
-import cn.edu.thssdb.rpc.thrift.GetTimeReq;
-import cn.edu.thssdb.rpc.thrift.GetTimeResp;
-import cn.edu.thssdb.rpc.thrift.IService;
-import cn.edu.thssdb.rpc.thrift.Status;
+import cn.edu.thssdb.rpc.thrift.*;
 import cn.edu.thssdb.server.ThssDB;
 import cn.edu.thssdb.utils.Global;
 import org.apache.thrift.TException;
@@ -44,8 +36,8 @@ public class IServiceHandler implements IService.Iface {
     }
 
     @Override
-    public DisconnetResp disconnect(DisconnetResp req) throws TException {
-        DisconnetResp resp = new DisconnetResp();
+    public DisconnectResp disconnect(DisconnectReq req) throws TException {
+        DisconnectResp resp = new DisconnectResp();
         resp.setStatus(new Status(Global.SUCCESS_CODE));
         return resp;
     }
